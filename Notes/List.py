e16b6cf1b3008db0856acdc1f9ab1b9ec048a24c
Mohit Bisht <mail.mohitbisht@gmail.com>
2024-01-27 22:23:41 +0530

    Create List.py

---
+++ Notes/List.py
@@ -0,0 +1,18 @@
+# List in Python
+
+l1 = ["Mohit", 45, "India", 33]
+print(l1)
+
+# it is mutable
+
+l1.remove("Mohit")
+print(l1)
+
+l1.pop()
+print(l1)
+
+l1.clear()
+print(l1)
+
+l1.extend([43, 34, 34, 34])
+print(l1)
