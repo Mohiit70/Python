010ec7006277e3647c605c5c5676356a15e9c1eb
Mohit Bisht <mail.mohitbisht@gmail.com>
2024-01-27 23:00:10 +0530

    Create if-else.py

---
+++ Notes/if-else.py
@@ -0,0 +1,17 @@
+# IF ELSE STATEMENT
+
+x = 20
+y = 30 
+
+if x>y:
+    print("False")
+else:
+    print("True")
+    
+age = int(input("enter your age: "))
+if age<18:
+    print("Your are not ready to go")
+elif age == 1:
+    print("NO")
+else:
+    print("You are ready to go")
