057185eb90b3e50bc8ffd8a67c0748f1a05bf597
Mohit Bisht <mail.mohitbisht@gmail.com>
2024-01-27 22:41:53 +0530

    Create Fun.py

---
+++ Notes/Fun.py
@@ -0,0 +1,13 @@
+# Function in Python
+
+def client():
+    print("Welcome to my Github")
+
+client()
+
+
+def client(name,mail):
+    str = f"Thank You {name} \n Your mail id {mail} is Saved in our Data"
+    print(str)
+    
+print(client("Mohit","mail@"))
