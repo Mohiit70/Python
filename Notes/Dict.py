b01bc5136815ed7b066173098b0caaac8da0449e
Mohit Bisht <mail.mohitbisht@gmail.com>
2024-01-27 22:19:21 +0530

    Create Dict.py

---
+++ Notes/Dict.py
@@ -0,0 +1,12 @@
+# Dictionary in Python
+
+Car = {"Mohit": 2, "Rohit": 4, "Nkhil": 6}
+print(Car)
+
+# its is mutable
+
+Car["Deep"] = 56
+print(Car)
+print(Car["Mohit"])
+print(Car.get("Deep"))
+print(type(Car))
