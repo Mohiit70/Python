d4f68bc9ea08f06846565089153e20b714241a16
Mohit Bisht <mail.mohitbisht@gmail.com>
2024-01-28 23:42:17 +0530

    Create Package.py

---
+++ Notes/Package.py
@@ -0,0 +1,13 @@
+# Packages in Python
+
+# What is Package ?
+
+# It is refer to build package where we can use the without writting it ie. we have pandas
+
+# So How to use ?
+
+import from pandas # import is keyword 
+
+# or 
+
+import from pandas as p # as is keyword to use pandas as s in our program instead of pandas we can simpliy write p or any other words
