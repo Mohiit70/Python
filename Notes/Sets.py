f4aaba6ba4f5f964ba1f54c947bd59a5dd7cab3b
Mohit Bisht <mail.mohitbisht@gmail.com>
2024-01-28 23:37:05 +0530

    Create Sets.py

---
+++ Notes/Sets.py
@@ -0,0 +1,9 @@
+# Sets in Python
+
+a = {3,4,5,24,24,24,24,55,66}
+print(a) # it will print only once an element
+
+b = {45,535,55,1,2,3}
+print(a.intersection(b))
+
+# we have different types of function such as union, pop, remove etc
